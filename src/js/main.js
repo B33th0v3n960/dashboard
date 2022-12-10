a50c136e967beb3bd3b046b71eca53f8f110383e
B33th0v3n960 <zhousun06@gmail.com>
2022-12-10 11:45:01 +0000

    fixed graph layout issues with js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,35 @@
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
+
+const guideLine = document.querySelectorAll(".guideline");
+
+const jobGraph = document.querySelector("#job-overview-graph");
+console.log(guideLine);
+console.log(jobGraph);
+
+function guidlineSize() {
+  //   console.log(graph.offsetWidth);
+  guideLine.forEach((element) => {
+    element.style.setProperty("--width", `${jobGraph.offsetWidth}px`);
+  });
+}
+
+guidlineSize();
+window.addEventListener("resize", guidlineSize);
+
+console.log(jobGraph.childElementCount);
+
+jobGraph.style.setProperty(
+  "--last-width",
+  `${25 / jobGraph.childElementCount}%`
+);
+
+const bar = document.querySelectorAll(".bar-container");
+console.log(bar);
+function positionBarLegend() {
+  bar.forEach((element) => {
+    element.style.setProperty("--move-right", `${element.offsetWidth}%`);
+  });
+}
+positionBarLegend();
+window.addEventListener("resize", positionBarLegend);
