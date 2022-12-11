3a701ebf1f9f343d5e4e17b2d6390d9670ce86a8
B33th0v3n960 <zhousun06@gmail.com>
2022-12-11 11:18:37 +0000

    clean code up, add comment, rename id and other attributes

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,38 @@
 // Import all of Bootstrap's JS
-import * as bootstrap from "bootstrap";
+import * as bootstrap from 'bootstrap'
 
-const guideLine = document.querySelectorAll(".guideline");
+const guideLine = document.querySelectorAll('.guideline')
+const jobGraph = document.querySelector('#job-overview-graph')
+const signUpGraph = document.querySelector('#sign-up-graph')
+const bar = document.querySelectorAll('.bar-container')
 
-const jobGraph = document.querySelector("#job-overview-graph");
-const signUpGraph = document.querySelector("#sign-up-graph");
-console.log(guideLine);
-console.log(jobGraph);
-
-function guidlineSize() {
-  //   console.log(graph.offsetWidth);
-  guideLine.forEach((element) => {
-    element.style.setProperty("--width", `${jobGraph.offsetWidth}px`);
-  });
-}
-
-guidlineSize();
-window.addEventListener("resize", guidlineSize);
-
-console.log(jobGraph.childElementCount);
-
+// set the length of the last td element in bar chart
 jobGraph.style.setProperty(
-  "--last-width",
+  '--last-width',
   `${25 / jobGraph.childElementCount}%`
-);
+)
 
 signUpGraph.style.setProperty(
-  "--last-width",
+  '--last-width',
   `${25 / signUpGraph.childElementCount}%`
-);
+)
 
-const bar = document.querySelectorAll(".bar-container");
-console.log(bar);
+// set horizontal guideline size to be equal to graph length
+guidlineSize()
+window.addEventListener('resize', guidlineSize)
+
+// set the local of the bottom legend to be under bar at all times
+positionBarLegend()
+window.addEventListener('resize', positionBarLegend)
+
+function guidlineSize() {
+  guideLine.forEach((element) => {
+    element.style.setProperty('--width', `${jobGraph.offsetWidth}px`)
+  })
+}
+
 function positionBarLegend() {
   bar.forEach((element) => {
-    element.style.setProperty("--move-right", `${element.offsetWidth}%`);
-  });
+    element.style.setProperty('--move-right', `${element.offsetWidth}%`)
+  })
 }
-positionBarLegend();
-window.addEventListener("resize", positionBarLegend);
