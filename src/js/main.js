c704668d8dbb128f9bde37d8456cbc7021ba9edc
B33th0v3n960 <zhousun06@gmail.com>
2022-12-11 15:54:33 +0000

    refactor and fix graph layout in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,49 @@
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap";
 
-const guideLine = document.querySelectorAll('.guideline')
-const jobGraph = document.querySelector('#job-overview-graph')
-const signUpGraph = document.querySelector('#sign-up-graph')
-const bar = document.querySelectorAll('.bar-container')
+const jobGraph = document.querySelector("#job-overview-graph");
+const signUpGraph = document.querySelector("#sign-up-graph");
+const bar = document.querySelectorAll(".bar-container");
 
 // set the length of the last td element in bar chart
 jobGraph.style.setProperty(
-  '--last-width',
+  "--last-width",
   `${25 / jobGraph.childElementCount}%`
-)
+);
 
 signUpGraph.style.setProperty(
-  '--last-width',
+  "--last-width",
   `${25 / signUpGraph.childElementCount}%`
-)
+);
 
 // set horizontal guideline size to be equal to graph length
-guidlineSize()
-window.addEventListener('resize', guidlineSize)
+jobGuidelineSize();
+signupGuidelineSize();
+window.addEventListener("resize", jobGuidelineSize);
+window.addEventListener("resize", signupGuidelineSize);
 
 // set the local of the bottom legend to be under bar at all times
-positionBarLegend()
-window.addEventListener('resize', positionBarLegend)
+positionBarLegend();
+window.addEventListener("resize", positionBarLegend);
 
-function guidlineSize() {
-  guideLine.forEach((element) => {
-    element.style.setProperty('--width', `${jobGraph.offsetWidth}px`)
-  })
+function jobGuidelineSize() {
+  const guideLine = document.querySelector("#job-legend").children;
+
+  for (let element of guideLine) {
+    element.style.setProperty("--width", `${jobGraph.offsetWidth}px`);
+  }
+}
+
+function signupGuidelineSize() {
+  const guideLine = document.querySelector("#signup-legend").children;
+
+  for (let element of guideLine) {
+    element.style.setProperty("--width", `${signUpGraph.offsetWidth}px`);
+  }
 }
 
 function positionBarLegend() {
   bar.forEach((element) => {
-    element.style.setProperty('--move-right', `${element.offsetWidth}%`)
-  })
+    element.style.setProperty("--move-right", `${element.offsetWidth}%`);
+  });
 }
