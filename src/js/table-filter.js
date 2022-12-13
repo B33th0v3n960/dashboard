9b9a0dbc4ced0ba4bb1cc191dbba501fe2c567aa
B33ht0v3n960 <zhousun06@gmail.com>
2022-12-13 16:53:24 +0000

    remove uneccessary console.logs

--- src/js/table-filter.js
+++ src/js/table-filter.js
@@ -7,7 +7,6 @@ export function filterTable(id, tbodyId, header, data) {
 
   for (let element of jobTable) {
     element.addEventListener('click', () => {
-      console.log(element.id)
       const result = filterOutput(id, header, data, element.id)
       const tbody = document.querySelector(tbodyId)
       tbody.innerHTML = result
@@ -16,7 +15,6 @@ export function filterTable(id, tbodyId, header, data) {
 }
 
 function filterOutput(id, header, data, key) {
-  console.log(key)
   const newData = sortObjectByKey(data, key)
   return tableBody(newData, header)
 }
@@ -24,9 +22,7 @@ function filterOutput(id, header, data, key) {
 function sortObjectByKey(arr, key) {
   // create a new array with the data from the original object
   const data = [...arr]
-  console.log(typeof arr[0][key])
-  console.log(!isNaN(arr[0][key]))
-  console.log(typeof data[0].key == 'number')
+
   // sort the array of data by the value of the given key
   if (!isNaN(arr[0][key])) data.sort((a, b) => a[key] - b[key])
   else data.sort((a, b) => a[key].localeCompare(b[key]))
