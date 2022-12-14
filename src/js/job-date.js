db0e426aab41337320744aa556747ab94ad3cb67
B33ht0v3n960 <zhousun06@gmail.com>
2022-12-14 12:54:30 +0000

    add data validation to date input for table

--- src/js/job-date.js
+++ src/js/job-date.js
@@ -7,12 +7,32 @@ export function changeTableDate(id, company) {
   input.addEventListener('input', () => {
     const monthJobData = job[input.value]
 
-    fillTable(
-      '#job-table',
-      '#job-thead',
-      '#job-tbody',
-      monthJobData.header,
-      monthJobData.data
-    )
+    const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+    const alert = (message, type) => {
+      const wrapper = document.createElement('div')
+      wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible m-2" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>',
+      ].join('')
+
+      alertPlaceholder.append(wrapper)
+    }
+
+    if (monthJobData)
+      fillTable(
+        '#job-table',
+        '#job-thead',
+        '#job-tbody',
+        monthJobData.header,
+        monthJobData.data
+      )
+    else
+      alert(
+        'The date you have chosen does is not available in our data base',
+        'success'
+      )
   })
 }
